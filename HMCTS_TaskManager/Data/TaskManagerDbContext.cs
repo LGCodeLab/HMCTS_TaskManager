1f5024e6e4c2910979614c4c2c21df6d93e89439
Luke Gray <lu.kee@hotmail.co.uk>
2026-05-08 14:18:26 +0100

    Set up EF core with SQLite db and initial migration.

--- HMCTS_TaskManager/Data/TaskManagerDbContext.cs
+++ HMCTS_TaskManager/Data/TaskManagerDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace HMCTS_TaskManager.Data
+﻿using HMCTS_TaskManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HMCTS_TaskManager.Data
 {
-    public class TaskManagerDbContext
+    public class TaskManagerDbContext : DbContext
     {
+        public TaskManagerDbContext(DbContextOptions<TaskManagerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TaskItem> Tasks { get; set; }
     }
 }
