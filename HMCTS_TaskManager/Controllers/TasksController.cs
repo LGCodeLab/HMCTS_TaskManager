dfa20accb86a00e5a6e1e40d345e25ac896d36b0
Luke Gray <lu.kee@hotmail.co.uk>
2026-05-11 14:13:55 +0100

    Added /Tasks endpoint to fetch and display tasks async.

--- HMCTS_TaskManager/Controllers/TasksController.cs
+++ HMCTS_TaskManager/Controllers/TasksController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HMCTS_TaskManager.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HMCTS_TaskManager.Controllers
 {
     public class TasksController : Controller
     {
-        public IActionResult Index()
+        private readonly TaskManagerDbContext _dbContext;
+
+        public TasksController(TaskManagerDbContext dbContext)
         {
-            return View();
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Models.TaskItem> tasks = await _dbContext.Tasks.ToListAsync();
+
+            return View(tasks);
         }
     }
 }
