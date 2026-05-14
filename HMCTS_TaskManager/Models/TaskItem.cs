9ee1a6b5ffa235db96374f7348ebd0468ea918b6
Luke Gray <lu.kee@hotmail.co.uk>
2026-05-14 14:57:57 +0100

    Added validation attributes to Model.

--- HMCTS_TaskManager/Models/TaskItem.cs
+++ HMCTS_TaskManager/Models/TaskItem.cs
@@ -1,15 +1,23 @@
-﻿namespace HMCTS_TaskManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HMCTS_TaskManager.Models
 {
     public class TaskItem
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(100)]
         public string Title { get; set; } = string.Empty;
 
+        [StringLength(500)]
         public string? Description { get; set; }
 
+        // TODO: Do we need the default value here also?
+        [Required]
         public string Status { get; set; } = "ToDo";
 
+        [Required]
         public DateTime DueDate { get; set; }
     }
 }
